4c7bfdccb70e2d669310b31f1202a5038a3e9d7b
Abdulrahman Mousa <amousa@mtcmea.com>
2021-07-17 23:58:37 +0300

    finazlie ar

--- scripts.js
+++ scripts.js
@@ -19,9 +19,23 @@ function changeName() {
 function setAr() {
 	document.getElementById('cardAr').hidden = false;
 	document.getElementById('cardEn').hidden = true;
+	document.getElementById('cardTextAr').hidden = false;
+	document.getElementById('cardTextEn').hidden = true;
+	// document.getElementById('cardTitleAr').hidden = false;
+	// document.getElementById('cardTitleEn').hidden = true;
+	document.getElementById('empNameLabel').innerText = "الاسم";
+	document.getElementById('empName').placeholder = "اسمك";
+	document.getElementById('downloadBtn').innerText = "تنزيل";
 }
 
 function setEn() {
 	document.getElementById('cardAr').hidden = true;
 	document.getElementById('cardEn').hidden = false;
+	document.getElementById('cardTextAr').hidden = true;
+	document.getElementById('cardTextEn').hidden = false;
+	// document.getElementById('cardTitleAr').hidden = true;
+	// document.getElementById('cardTitleEn').hidden = false;
+	document.getElementById('empNameLabel').innerText = "Name";
+	document.getElementById('empName').placeholder = "Your name";
+	document.getElementById('downloadBtn').innerText = "Download";
 }
